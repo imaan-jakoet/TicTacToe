48cbb5ae6da3c652e260f9828905c7168dd8dc5d
imaan-jakoet <imaanjakoet1@gmail.com>
2026-02-04 12:01:20 +0200

    created gameLogic and gameBoard classes

---
+++ Business/GameBoard.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TicTacToe.Business
+{
+    public class GameBoard
+    {
+        public void newBoard() {
+            private enum CellState
+        {
+            EMPTY,
+            X,
+            O
+        }
+        private var grid = new List<KeyValuePair<int, enum>>
+            {
+                for (int i = 0, int<10, int++){
+                    new KeyValuePair<int, string>(i, CellState.EMPTY),
+                }
+            }
+        }
+    }
+}
